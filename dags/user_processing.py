c1c36c98a3679f7a54aa95dbf59c5f4fbc13a538
GS <gastonsingh@hotmail.com>
2023-01-18 11:22:05 -0300

    SimpleHttpOperator

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,7 +1,9 @@
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
 
+import json
 from datetime import datetime
 
 with DAG('user_processing', start_date=datetime(2022, 1, 1),
@@ -24,4 +26,13 @@ with DAG('user_processing', start_date=datetime(2022, 1, 1),
         task_id='is_api_available',
         http_conn_id='user_api',
         endpoint='api/'
+    )
+
+    extract_user = SimpleHttpOperator(
+        task_id='extract_user',
+        http_conn_id='user_api',
+        endpoint='api/',
+        method='GET',
+        response_filter=lambda response: json.loads(response.text),
+        log_response=True
     )
