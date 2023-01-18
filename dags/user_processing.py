46396d1c3a09e006f057fe6ed7fb78ab223b4bc5
GS <gastonsingh@hotmail.com>
2023-01-18 11:20:06 -0300

    HttpSensor

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.http.sensors.http import HttpSensor
 
 from datetime import datetime
 
@@ -18,3 +19,9 @@ with DAG('user_processing', start_date=datetime(2022, 1, 1),
                 email TEXT NOT NULL
             );
         ''')
+
+    is_api_available = HttpSensor(
+        task_id='is_api_available',
+        http_conn_id='user_api',
+        endpoint='api/'
+    )
