eed4c9b22db9023b956cd9c692e7e8b97353b3bb
GS <gastonsingh@hotmail.com>
2023-01-21 20:37:45 -0300

    many datasets

--- dags/consumer.py
+++ dags/consumer.py
@@ -4,10 +4,11 @@ from airflow.decorators import task
 from datetime import datetime
 
 my_file = Dataset("/tmp/my_file.txt")
+my_file_2 = Dataset("/tmp/my_file_2.txt")
 
 with DAG(
     dag_id="consumer",
-    schedule=[my_file],
+    schedule=[my_file, my_file_2],
     start_date=datetime(2022, 1, 1),
     catchup=False
 ):
--- dags/producer.py
+++ dags/producer.py
@@ -4,6 +4,7 @@ from airflow.decorators import task
 from datetime import date, datetime
 
 my_file = Dataset("/tmp/my_file.txt")
+my_file_2 = Dataset("/tmp/my_file_2.txt")
 
 with DAG(
     dag_id="producer",
@@ -17,4 +18,9 @@ with DAG(
         with open(my_file.uri, "a+") as f:
             f.write("producer update")
 
-    update_dataset()
+    @task(outlets=[my_file_2])
+    def update_dataset_2():
+        with open(my_file_2.uri, "a+") as f:
+            f.write("producer update")
+
+    update_dataset() >> update_dataset_2()
