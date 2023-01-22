22d00787bcb4b54a882f064b825ad4dc8796bf1c
GS <gastonsingh@hotmail.com>
2023-01-21 21:15:51 -0300

    fix task sleep

--- dags/parallel_dag.py
+++ dags/parallel_dag.py
@@ -7,27 +7,27 @@ with DAG('parallel_dag', start_date=datetime(2022, 1, 1),
          schedule_interval='@daily', catchup=False) as dag:
     extract_a = BashOperator(
         task_id='extract_a',
-        bash_command='sleep 1'
+        bash_command='sleep 10'
     )
 
     extract_b = BashOperator(
         task_id='extract_b',
-        bash_command='sleep 1'
+        bash_command='sleep 10'
     )
 
     load_a = BashOperator(
         task_id='load_a',
-        bash_command='sleep 1'
+        bash_command='sleep 10'
     )
 
     load_b = BashOperator(
         task_id='load_b',
-        bash_command='sleep 1'
+        bash_command='sleep 10'
     )
 
     transform = BashOperator(
         task_id='transform',
-        bash_command='sleep 1'
+        bash_command='sleep 10'
     )
 
     extract_a >> load_a
